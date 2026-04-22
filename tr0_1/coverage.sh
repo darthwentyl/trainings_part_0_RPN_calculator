41d83aa42008d582ceb02160a4eb0e07c0242749
darthwentyl <michal.wendland@accenture.com>
2026-04-22 15:45:58 +0200

    Improvment coverage.sh script - generate report for only source code .gitignore all build* directories

--- tr0_1/coverage.sh
+++ tr0_1/coverage.sh
@@ -2,16 +2,18 @@
 
 BUILD_DIR="build_coverage"
 OUTPUT_STAT="coverage"
+NUMBER_OF_CPUS="$(nproc --all)"
 if [ -d ${BUILD_DIR} ]; then
     rm -rf ${BUILD_DIR}
 fi
 mkdir -p ${BUILD_DIR}
 cd ${BUILD_DIR}
 cmake -DCMAKE_BUILD_TYPE=coverage ..
-make -j8
+make -j${NUMBER_OF_CPUS}
 ./test/complex_calc_tests
 
 find . -name '*.gcda' | xargs gcov
 lcov --ignore mismatch --directory . --capture --output-file coverage.info
-genhtml -o ${OUTPUT_STAT} coverage.info
+lcov --extract coverage.info '*/src/*' --output-file coverage.src-only.info
+genhtml -o ${OUTPUT_STAT} coverage.src-only.info
 firefox ${OUTPUT_STAT}/index.html &> /dev/null &
--- tr0_3/calculator_tests/coverage.sh
+++ tr0_3/calculator_tests/coverage.sh
@@ -2,15 +2,18 @@
 
 BUILD_DIR="build_coverage"
 OUTPUT_STAT="coverage"
+NUMBER_OF_CPUS="$(nproc --all)"
 if [ -d ${BUILD_DIR} ]; then
     rm -rf ${BUILD_DIR}
 fi
 mkdir -p ${BUILD_DIR}
 cd ${BUILD_DIR}
 cmake -DCMAKE_BUILD_TYPE=coverage ..
-make -j8
+make -j${NUMBER_OF_CPUS}
 ./calculator_tests
 find . -name '*.gcda' | xargs gcov -d
 lcov --ignore mismatch --directory . --capture --output-file coverage.info
-genhtml -o ${OUTPUT_STAT} coverage.info
+lcov --extract coverage.info '*/calculator/*' --output-file coverage.src-all.info
+lcov --remove coverage.src-all.info '*/lib/*' --output-file coverage.src-only.info
+genhtml -o ${OUTPUT_STAT} coverage.src-only.info
 firefox ${OUTPUT_STAT}/index.html &> /dev/null &
